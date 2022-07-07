31e1b3e6a0b0e2f541ec0f29135493506f3f1281
Сергей Соболев <sergey.sobolev1103@gmail.com>
2022-07-07 17:55:08 +0300

    Editing main function

--- Practice_1course/main.c
+++ Practice_1course/main.c
@@ -1,6 +1,21 @@
 #include "header.h"
 
 int main() {
+	struct point A, B, C, POINT;
+
+	struct A* pa;
+	pa = &A;
+	struct B* pb;
+	pb = &B;
+	struct C* pc;
+	pc = &C;
+	struct POINT* pp;
+	pp = &POINT;
+
+	printf("Hello! This program checks if a point belongs to a triangle.");
+	while (true) {
+		
+	}
 
 	return 0;
 }
